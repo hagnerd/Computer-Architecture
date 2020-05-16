e29276de84b46af1cb00144efbc3ee8752a3c748
Matt Hagner <web@matthagner.com>
2020-05-16 12:01:33 -0500

    Complete call and ret

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -11,8 +11,23 @@ LDI = 0b10000010
 PRN = 0b01000111
 # MUL
 MUL = 0b10100010
+ADD = 0b10100000
 PUSH = 0b01000101
 POP = 0b01000110
+CALL = 0b01010000
+RET = 0b00010001
+
+CMDS = {
+    0b00000001: 'HLT',
+    0b10000010: 'LDI',
+    0b01000111: 'PRN',
+    0b10100010: 'MUL',
+    0b01000101: 'PUSH',
+    0b01000110: 'POP',
+    0b01010000: 'CALL',
+    0b00010001: 'RET',
+    0b10100000: 'ADD'
+}
 
 
 class CPU:
@@ -82,11 +97,20 @@ class CPU:
         from run() if you need help debugging.
         """
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
+        counter = self.pc
+
+        if self.ram_read(counter) not in CMDS:
+            print("INVALID COMMAND FOUND:")
+            print(f"PC: {counter}, val: {self.ram_read(counter)}")
+
+            self.running = False
+            return 
+
+        print(f"TRACE: %02X | %s %02X %02X |" % (
             self.pc,
             #self.fl,
             #self.ie,
-            self.ram_read(self.pc),
+            CMDS[self.ram_read(self.pc)],
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
         ), end='')
@@ -115,33 +139,80 @@ class CPU:
 
         self.pc += 2
 
-    def stack_push(self):
+    def _stack_push(self, value):
         """
         Adds an item to the stack
         """
-        register = self.ram_read(self.pc + 1)
-        value = self.reg[register]
         self.dec_stack_pointer()
         self.ram_write(self.get_stack_pointer(), value)
-        self.pc += 2
 
 
-    def stack_pop(self):
+    def _stack_pop(self):
         """
         Removes an item from the stack
         """
         # Retrieve the value from RAM at the address in SP
         # Store the value at the register passed in
         # Increment SP
+        val = self.ram_read(self.get_stack_pointer())
+        self.inc_stack_pointer()
+        return val
 
+    def handle_stack_pop(self):
         register = self.ram_read(self.pc + 1)
-
-        if self.get_stack_pointer() < len(self.ram):
-            value = self.ram_read(self.get_stack_pointer())
-            self.reg[register] = value
-            self.inc_stack_pointer()
-
+        val = self._stack_pop()
+        self.reg[register] = val
         self.pc += 2
+
+
+    def handle_stack_push(self):
+        register = self.ram_read(self.pc + 1)
+        value = self.reg[register]
+        self._stack_push(value)
+        self.pc += 2
+
+    def handle_call(self):
+        # pc = CALL instruction
+        # pc+1 = address of subroutine
+        # Place the address of the next instruction on the stack
+        # Set the PC to the address of the subroutine
+
+        next_instruction = self.pc + 2
+        subroutine = self.ram_read(self.pc + 1)
+        sub_address = self.reg[subroutine]
+
+        print(f"NEXT: {next_instruction}, SUB: {subroutine}, ADD: {sub_address}")
+
+        self._stack_push(next_instruction)
+        self.pc = self.reg[subroutine]
+
+    def handle_ret(self):
+        # Pop the next instruction off of the stack
+
+        next_instruction = self._stack_pop()
+        self.pc = next_instruction
+
+    def handle_cmd(self, command):
+        if command == HLT:
+            self.hlt()
+        elif command == LDI:
+            self.ldi()
+        elif command == PRN:
+            self.prn()
+        elif command == MUL:
+            self.alu('MUL', self.ram_read(self.pc + 1),
+                     self.ram_read(self.pc + 2))
+        elif command == ADD:
+            self.alu('ADD', self.ram_read(self.pc + 1), self.ram_read(self.pc +
+                                                                      2))
+        elif command == PUSH:
+            self.handle_stack_push()
+        elif command == POP:
+            self.handle_stack_pop()
+        elif command == CALL:
+            self.handle_call()
+        elif command == RET:
+            self.handle_ret()
 
 
     def run(self):
@@ -151,21 +222,5 @@ class CPU:
 
         while self.running:
             command = self.ram[self.pc]
-
-            # HALT
-            if command == HLT:
-                self.hlt()
-            # LDI what does that even mean???
-            elif command == LDI:
-                self.ldi()
-            # PRN
-            elif command == PRN:
-                self.prn()
-            elif command == MUL:
-                self.alu('MUL', self.ram_read(self.pc + 1),
-                         self.ram_read(self.pc + 2))
-            elif command == PUSH:
-                self.stack_push()
-            elif command == POP:
-                self.stack_pop()
-
+            self.trace()
+            self.handle_cmd(command)
